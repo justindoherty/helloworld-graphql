33b929019318423f0c5b3369511385f4f92361bc
Justin Doherty <justin.doherty@solarwinds.com>
2017-02-18 17:37:11 -0500

    Accept CORS and return json

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,6 +6,7 @@ import graphql.GraphQL;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,6 +21,7 @@ public class Main {
                     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
 
     public static void main(String[] args) {
+        Main.enableCORS("http://localhost:4200", "POST", "");
         GraphQLObjectType queryType = newObject()
                 .name("helloWorldQuery")
                 .field(newFieldDefinition()
@@ -39,6 +41,9 @@ public class Main {
             payload = mapper.get().readValue(request.body(), Map.class);
             Map<String,Object> variables =
                     (Map<String, Object>) payload.get("variables");
+            if (variables == null) {
+                variables = Collections.emptyMap();
+            }
             ExecutionResult executionResult =
                     graphql.execute(payload.get("query").toString(), null, null, variables);
             Map<String, Object> result = new LinkedHashMap<>();
@@ -47,7 +52,33 @@ public class Main {
             }
             result.put("data", executionResult.getData());
             response.type("application/json");
-            return result;
+            return mapper.get().writeValueAsString(result);
+        });
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
         });
     }
 }
